f7e63aef8a6bcf8baa0b934bee2a34176fc838eb
Andrea Napoli <andreanapoli8801@gmail.com>
2025-10-16 15:57:30 +0200

    aggiunto componente login

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home.component';
 //import { AuthGuard } from './auth.guard'; 
+import { Login } from './login/login.component';
 
 export const routes: Routes = [
   //{ path: 'login', component: LoginComponent },
   { path: 'home', component: Home},
+  { path: 'login', component: Login },
   //{ path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
---
+++ front-end/src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class Login {
+  email = '';
+  password = '';
+  loginFailed: boolean = false;
+  passwordVisible = false;
+
+  //constructor(private authService: AuthService, private router: Router) {}
+
+  /*onSubmit() {
+    if (this.username === '' || this.password === '') {
+      alert('Compilare tutti i campi');
+    } else {
+      const success = this.authService.login(this.username, this.password);
+      if (success) {
+        this.router.navigate(['/home']); // Reindirizza alla pagina home se il login ha avuto successo
+      } else {
+        alert('Utente non trovato');
+        this.loginFailed = true;
+      }
+    }
+  }*/
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
+}
+
