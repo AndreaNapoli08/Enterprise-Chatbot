cf87b1db95b07dadade2d8d87322fb506f8a4ae3
Andrea Napoli <andreanapoli8801@gmail.com>
2025-11-14 19:16:57 +0100

    implementazione sidebar con storico chat

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, AfterViewChecked } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewChecked, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputText } from '../input-text/input-text.component';
 import { ChatBubble } from '../chat-bubble/chat-bubble.component';
@@ -45,7 +45,38 @@ export class Home implements AfterViewChecked {
     'Sto cercando una risposta...'
   ];
 
+  // menu rinomina o cancella chat
+  openMenu: number | null = null;
+  dropdownTop = 0;
+  dropdownLeft = 0;
+
+  // simulazione diverse chat
+  chatTitles: string[] = [
+    'Prenotazione stanza',
+    'Supporto clienti',
+    'Help Desk',
+    'Ordine prodotto',
+    'Aggiornamento spedizione',
+    'Appuntamento medico',
+    'Check-in hotel',
+    'Risoluzione problemi',
+    'Conferma prenotazione',
+    'Pagamenti e fatture',
+    'Promozioni e offerte',
+    'Resi e rimborsi',
+    'Riunione progetto',
+    'Task e attività',
+    'Annunci aziendali',
+    'Brainstorming team',
+    'Chat familiare',
+    'Gruppo amici',
+    'Suggerimenti',
+    'Domande frequenti'
+  ];
+
   @ViewChild('scrollContainer') private scrollContainer!: ElementRef<HTMLDivElement>;
+  @ViewChild('drawer') drawer!: ElementRef;
+
   constructor(
     private authService: AuthService,
     private chatService: ChatService
@@ -84,6 +115,43 @@ export class Home implements AfterViewChecked {
     } catch (err) {
       console.warn('Scroll error:', err);
     }
+  }
+
+  openDrawer() {
+    const el = this.drawer.nativeElement;
+    el.classList.remove('-translate-x-full');
+  }
+
+  closeDrawer() {
+    const el = this.drawer.nativeElement;
+    el.classList.add('-translate-x-full');
+  }
+
+  openDropdown(event: MouseEvent, trigger: HTMLElement, index: number) {
+    event.stopPropagation();
+
+    const rect = trigger.getBoundingClientRect();
+    const dropdownHeight = 80; // altezza stimata del menu dropdown
+    const margin = 4; // margine tra bottone e dropdown
+    const windowHeight = window.innerHeight;
+
+    // Calcola se c'è spazio sotto
+    if (rect.bottom + dropdownHeight + margin > windowHeight) {
+      // Non c'è abbastanza spazio sotto: apri sopra
+      this.dropdownTop = rect.top - dropdownHeight - margin;
+    } else {
+      // C'è spazio: apri sotto
+      this.dropdownTop = rect.bottom + margin;
+    }
+
+    // Allinea a destra del bottone
+    this.dropdownLeft = rect.right - 160;
+    this.openMenu = this.openMenu === index ? null : index;
+  }
+
+  @HostListener('document:click')
+  close() {
+    this.openMenu = null;
   }
 
   handleMessage(message: any): void {
